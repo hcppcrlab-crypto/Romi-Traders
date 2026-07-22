18fa1ecc921f31735abd1033738b46ccce1eee34
hcppcrlab-crypto <hcppcrlab@gmail.com>
2026-07-22 12:27:09 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  compress: true,
+
+  productionBrowserSourceMaps: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  },
+
+  experimental: {
+    typedRoutes: true
+  }
+};
 
 export default nextConfig;
